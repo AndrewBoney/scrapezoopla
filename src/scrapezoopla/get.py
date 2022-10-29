85a3c176586398023a553fc2985a597e9f8074ad
AB <andrew.boney@hotmail.co.uk>
2022-10-29 16:20:54 +0100

    add documentation to get.py

--- src/scrapezoopla/get.py
+++ src/scrapezoopla/get.py
@@ -14,6 +14,12 @@ In my head this module will do 3 things:
 
 class GetAll:
     def __init__(self, search_term:str, init_get: bool = False):
+        """Get info about all properties in a given search term.  
+
+        Args:
+            search_term (str): to search in zoopla
+            init_get (bool, optional): if True, will download data on initialisation. Defaults to False.
+        """
         self.search_term = search_term
         self.timestamp = round(datetime.now().timestamp()) # used for file name
         if init_get:
@@ -21,13 +27,23 @@ class GetAll:
             self.get_data()
         
     def get_ids(self):
+        """Get property ids for the search
+        """
         self.search = search_zoopla(self.search_term)
         self.ids = get_property_ids(self.search)
 
     def get_data(self):
+        """Get data for all properties in the search
+        """
         self.data = [ScriptData(i).out_dict for i in self.ids]
 
     def write_local(self, base_path: str):
+        """Save property data locally as json. 
+        DEPRECATED (see write_s3). Might delete later.
+
+        Args:
+            base_path (str): path to save
+        """
         for (i, d) in zip(self.ids, self.data):
             path = os.path.join(base_path, i)
             os.makedirs(path, exist_ok = True)
@@ -36,6 +52,12 @@ class GetAll:
             with open(os.path.join(path, f"{self.timestamp}_data.json"), "w") as f: f.write(json_obj)
 
     def write_s3(self, local_path: str, s3_path: str):
+        """Write data to an s3 bucket
+
+        Args:
+            local_path (str): _description_
+            s3_path (str): _description_
+        """
         bucket = boto3.resource("s3").Bucket("zooplaproperties")
 
         for (i, d) in zip(self.ids, self.data):
